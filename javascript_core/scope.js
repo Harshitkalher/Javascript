2fa49603aadd27316c4f40ef0e5ff763724018e0
Harshitkalher <harshitkumar.eng@gmail.com>
2025-10-05 06:53:35 +0000

    add config done

--- javascript_core/scope.js
+++ javascript_core/scope.js
@@ -1,1 +1,10 @@
-console.log("Harshit kalher")
+function one (){ // things out from the scope is available within scope but vice versa is not true
+    const username = "Harshit" // inside the block is known as block scope or local scope 
+    function two (){ // these curly braces are the scope , that defines boundaries of the things within it
+        const college = "MIET"
+        console.log(username) //inner scope fetch value from outer  scope , but vice versa is not true
+        //just like little child take icecream from elder one but elder one not
+    }
+    two()
+}
+one()
